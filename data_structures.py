b70760e8711d74720944bf2dd620b5b103c721e6
Qiniso Xulu <qiniso.nhlakanipho.xulu@gmail.com>
2025-10-02 13:32:44 +0200

    first commit

---
+++ data_structures.py
@@ -0,0 +1,44 @@
+import random
+
+def find_max(numbers: list):
+    """Find the largest(maximum) number in a list of numbers"""
+    pass
+
+def find_min(numbers: list):
+    """Find the smallest(minimum) number in a list of numbers"""
+    pass
+
+def find_average(numbers: list):
+    """Find the average of a list of numbers int the list 'numbers' and return 
+    it as a float to one decimal point"""
+    pass
+
+def find_even_pairs(numbers: list):
+    """Find the neigbouring pairs of numbers that sum up to an even number and 
+    then return a list of tuples with the pairs' index numbers 
+    ie: [1,3,5] returns [(0,1)]
+    """
+    pass
+
+def find_number_of_even_numbers(numbers):
+    """Find the total number of even numbers in the list 'numbers' and return 
+    the number as an integer"""
+    pass
+
+
+def draw_square(height: int) -> None:
+    """
+    Draws a square pattern of asterisks (*) with the given height and width.
+
+    Parameters:
+        height (int): The height and width of the square. Must be a positive integer.
+
+    Returns:
+        None: Prints the square pattern directly to console.
+
+    """
+    pass
+
+
+if "__name__" == "__main__":
+    pass
