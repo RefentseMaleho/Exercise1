0fc18c86341ee2f228e0e2c01e5e81d75281038e
Qiniso Xulu <qixulujhb024@student.wethinkcode.co.za>
2025-10-03 09:50:22 +0200

    Fixing tests

--- data_structures.py
+++ data_structures.py
@@ -1,26 +1,26 @@
 import random
 
-def find_max(numbers: list):
+def find_max(numbers: list) -> int:
     """Find the largest(maximum) number in a list of numbers"""
     pass
 
-def find_min(numbers: list):
+def find_min(numbers: list) -> int:
     """Find the smallest(minimum) number in a list of numbers"""
     pass
 
-def find_average(numbers: list):
+def find_average(numbers: list) -> int:
     """Find the average of a list of numbers int the list 'numbers' and return 
     it as a float to one decimal point"""
     pass
 
-def find_even_pairs(numbers: list):
+def find_even_pairs(numbers: list) -> int:
     """Find the neigbouring pairs of numbers that sum up to an even number and 
     then return a list of tuples with the pairs' index numbers 
     ie: [1,3,5] returns [(0,1)]
     """
     pass
 
-def find_number_of_even_numbers(numbers):
+def find_number_of_even_numbers(numbers: list) -> int:
     """Find the total number of even numbers in the list 'numbers' and return 
     the number as an integer"""
     pass
